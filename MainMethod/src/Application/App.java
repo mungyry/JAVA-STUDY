3971bd1d802de4b6d78d4d43f2d4435de481bef4
pmk <mungyry1@naver.com>
2021-11-17 15:27:13 +0900

    11월17일 오후

---
+++ MainMethod/src/Application/App.java
@@ -0,0 +1,12 @@
+package Application;
+
+public class App {
+
+	public static void main(String[] args) {
+		// TODO Auto-generated method stub
+		System.out.println("�ȳ��ϼ���");
+		System.out.println("�� �ڹ� ���α׷���");
+		System.out.println("���� �մϴ�.");
+	}
+
+}
