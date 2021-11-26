5bd791943e08ad0b6a88e77b0b547e8b1d7bd0e8
pmk <mungyry1@naver.com>
2021-11-26 15:21:17 +0900

    11월 26일 오후

--- CH11_GameEx/src/game/Game.java
+++ CH11_GameEx/src/game/Game.java
@@ -1,6 +1,8 @@
 package game;
 
 import java.util.Random;
+import java.util.Scanner;
+
 import game.objects.GameObject;
 import game.objects.Paper;
 import game.objects.Rock;
@@ -10,16 +12,47 @@ public class Game {
 	//사용할 가위, 바위, 보 객체를 배열로 생성
 	GameObject[] objects = { new Scissors(), new Rock(), new Paper()};
 	Random random = new Random();
+	Scanner scanner = new Scanner(System.in);
 	
 	
 	public void run() {
-		System.out.println("게임시작");
-//		for(GameObject ob : objects) {
-//			System.out.println(ob.toString());
+		System.out.println("삼세판 게임시작");
+			int wins =0;
 			
-			int num =random.nextInt(objects.length);
-			GameObject ob = objects[num];
-			System.out.println(ob);
+			for(int i=1; i<=3; i++) { 
+				 // 유저의 선택
+			    System.out.println("가위(0), 바위(1), 보(2) 중 숫자로 선택 : " );
+				
+			    GameObject ob1 = objects[scanner.nextInt()];
+				GameObject ob2 = objects[random.nextInt(objects.length)];
+				
+				System.out.println("당신의 선택은 : " + ob1.getName());
+				System.out.println("컴퓨터 선택은 : " + ob2.getName());
+
+				int result = ob1.compareTo(ob2);
+				wins += result;
+				if(result > 0 ) {
+					System.out.println("당시의 승리");
+				} else if (result < 0){
+					System.out.println("당신의 패배");
+				} else {
+					System.out.println("비김");
+				}
+			}
+			// 3번 가위바위보 끝낸후
+			// 총점 wins로 전체 결과를 출력
+			System.out.println();
+			System.out.println("삼세판 결과는 : ");
+			
+			if(wins > 0 ) {
+				System.out.println("당시의 승리");
+			} else if (wins < 0){
+				System.out.println("당신의 패배");
+			} else {
+				System.out.println("비김");
+			}		
+	      
+			
 		}
 		
 	}
--- CH11_GameEx/src/game/objects/GameObject.java
+++ CH11_GameEx/src/game/objects/GameObject.java
@@ -3,6 +3,17 @@ package game.objects;
 public class GameObject {
 	private int id; // 아이디
 	private String name; //이름
+	private int[][] comparison = {
+		      // 가위 바위 보
+				{ 0, -1, 1}, // 가위 0
+				{ 1, 0, -1}, // 바위 1
+				{-1 , 1, 0}, //  보 2
+		};
+	// 상대의 (가위, 바위, 보)와 비교해서 결과 리턴
+	public int compareTo(GameObject ob) {
+		return comparison[id][ob.id];
+	}
+	
 	
 	
 	protected int getId() { // 아이디 수정은 상속한 클래스에세만 사용
