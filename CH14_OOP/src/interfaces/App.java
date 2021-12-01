89810d758677a2a5b9fbec639a6c3fb16d09e636
pmk <mungyry1@naver.com>
2021-12-01 15:13:57 +0900

    12월 1일 오후

---
+++ CH14_OOP/src/interfaces/App.java
@@ -0,0 +1,18 @@
+package interfaces;
+
+public class App {
+
+	public static void main(String[] args) {
+		// 인터페이스
+//		Describable ds = new Describable(); 객체를 만들 수 없다.
+		
+		Describable[] objs = {new Person(), new Computer() };
+		
+		for(Describable ob : objs) {
+			System.out.println(ob.getDescription());
+		}
+		
+
+	}
+
+}
---
+++ CH14_OOP/src/interfaces/Describable.java
@@ -0,0 +1,7 @@
+package interfaces;
+
+public interface Describable { // 인터페이스
+	 String getDescription(); // 주상 메소드로 인터페이스 안의 메소드는 public abstract
+	 
+
+}
