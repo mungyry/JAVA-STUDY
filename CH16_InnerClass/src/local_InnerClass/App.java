334c5f3dffba88571703776b1cb2755e65c501a1
pmk <mungyry1@naver.com>
2021-12-02 15:12:09 +0900

    12월 2일 오후

--- CH16_InnerClass/src/local_InnerClass/App.java
+++ CH16_InnerClass/src/local_InnerClass/App.java
@@ -33,6 +33,10 @@ public class App {
 //		new Printer().print();
 		
 		ScheduledExecutorService service = Executors.newScheduledThreadPool(1);
-		service.scheduleAtFixedRate(new Printer(), 0L, 1L, TimeUnit.SECONDS);
+		service.scheduleAtFixedRate(new Runnable() {
+			public void run() {
+				System.out.println(name);
+			}
+		}, 0L, 1L, TimeUnit.SECONDS);
 	}
 	}
