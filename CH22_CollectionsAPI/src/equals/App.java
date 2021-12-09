a0f5ab62da4deac43eb28bc2bf86d30abe32918a
ilho <sinflys@naver.com>
2021-12-09 11:12:23 +0900

    12월 9일 오전

---
+++ CH22_CollectionsAPI/src/equals/App.java
@@ -0,0 +1,41 @@
+package equals;
+
+import java.util.Objects;
+
+class Person {
+	private String name;
+	public Person(String name) {
+		this.name = name;
+	}
+	public String toString() {
+		return name;
+	}
+	@Override
+	public int hashCode() {
+		return Objects.hash(name);
+	}
+	@Override
+	public boolean equals(Object obj) {
+		if (this == obj) // 객체가 같을 경우
+			return true;
+		if (obj == null)
+			return false;
+		if (getClass() != obj.getClass())
+			return false;
+		Person other = (Person) obj;
+		return Objects.equals(name, other.name);
+	}
+	
+}
+public class App {
+	public static void main(String[] args) {
+		// 같은지 비교하는 메소드
+		Person p1 = new Person("펭수");
+		Person p2 = new Person("펭수");
+		
+		System.out.println(p1 == p2);
+		System.out.println(p1.equals(p2));
+
+	}
+
+}
