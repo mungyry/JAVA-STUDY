9ff266b253afaae79c6f9418536efa4ed7430630
ilho <sinflys@naver.com>
2021-12-09 10:47:26 +0900

    12월 9일 오전

--- CH22_CollectionsAPI/src/arrayList/Custom_Object.java
+++ CH22_CollectionsAPI/src/arrayList/Custom_Object.java
@@ -11,10 +11,12 @@ public class Custom_Object {
 		// 1. 객체를 생성하여 입력
 		Person p1 = new Person("펭수");
 		Person p2 = new Person("길동");
-		
-		// 2. 생성과 동시에 입력
 		people.add(p1);
 		people.add(p2);
+		
+		// 2. 생성과 동시에 입력
+		people.add(new Person("라이언"));
+		people.add(new Person("둘리"));
 		
 		people.forEach(System.out::println);
 	}
--- CH22_CollectionsAPI/src/sorting_list/Person.java
+++ CH22_CollectionsAPI/src/sorting_list/Person.java
@@ -1,6 +1,6 @@
 package sorting_list;
 
-//정렬을 할수있께 Comparable 비교가능한 
+//정렬을 할수있께 Comparable 비교가능한 인터페이스 구현
 public class Person implements Comparable<Person>  {
 	private String name;
 	
@@ -16,6 +16,7 @@ public class Person implements Comparable<Person>  {
 	public int compareTo(Person o) {
 		// 객체끼리 비교해서 누가 큰지 코드 작성
 		return name.compareTo(o.name);
+		// 문자열까지 비교하는 메소드 compareTo 사용하여 이름으로 비교
 		
 	}
 }
